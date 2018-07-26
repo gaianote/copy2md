a4d65f4fafc37b5cb7d9841efcf772798ec073f0
gaianote <gaianote@163.com>
2018-07-26 14:10:26 +0800

    fix empty tag reg rules

--- js/html2md.js
+++ js/html2md.js
@@ -1,4 +1,4 @@
-empty_node = /(?<=>)[\s]+?(?=<)/ //匹配html标签之间的空白字符
+empty_node = /(?<=<.*?>)[\s]+?(?=<\.*>)///匹配html标签之间的空白字符
 h1 = /<h1.*?>([\s\S]*?)<\/h1>/
 h2 = /<h2.*?>([\s\S]*?)<\/h2>/
 h3 = /<h3.*?>([\s\S]*?)<\/h3>/
@@ -16,13 +16,14 @@ tr = /<\/tr>/
 thead = /<\/thead>/
 link = /<a [\s\S]+?href="(.*?)".*?>(.+?)<\/a>/
 //处理pre 因为pre里面的换行是有意义的
-tag_in_pre = /<pre.*?>[\s\S]*?(<span.*?>|<\/span>)[\s\S]*?<\/pre.*?>/
+span = /<span.*?>|<\/span>/
 pre = /<pre.*?>|<\/pre.*?>/
 code = /<code.*?>|<\/code.*?>/
 other_tag = /<.*?>|<\/.*?>/
-
+html_r = '&gt;'
+html_l = '&lt;'
 //将所有匹配规则放入数组reg,注意排序顺序不能改变
-const reg_list = [tag_in_pre,empty_node,h1,h2,h3,h4,h5,h6,b,italic,li,p,br,tr,td,thead,img,link,pre,code,other_tag]
+const reg_list = [span,empty_node,h1,h2,h3,h4,h5,h6,b,italic,li,p,br,tr,td,thead,img,link,pre,code,other_tag,html_l,html_r]
 
 
 var html2md = function(outerHTML){
@@ -46,7 +47,9 @@ var replace = function(outerHTML,result,item){
   if(item == b){return outerHTML.replace(result[0],'**'+ result[1].trim() + '**')}
   if(item == italic){return outerHTML.replace(result[0],'*' + result[1].trim() + '*')}
   if(item == pre){return outerHTML.replace(result[0],'\n' + '```'+ '\n')}
-  if(item == tag_in_pre){return outerHTML.replace(result[0],result[0].replace(result[1],''))}
+  if(item == span){
+    console.log('span:' + outerHTML.replace(result[0],''))
+    return outerHTML.replace(result[0],'')}
   if(item == code){return outerHTML.replace(result[0],'`')}
   if(item == img){
     return outerHTML.replace(result[0],'![img](' + result[1] + ')')}
@@ -58,6 +61,39 @@ var replace = function(outerHTML,result,item){
   if(item == br){return outerHTML.replace(result[0],'\n')}
   if(item == p){return outerHTML.replace(result[0],'\n')}
   if(item == other_tag){return outerHTML.replace(result[0],'')}
-  if(item == empty_node){return outerHTML.replace(result[0],'')}
+  if(item == empty_node){
+    console.log('empty_node:' + outerHTML.replace(result[0],''))
+    return outerHTML.replace(result[0],'')}
+  if(item == html_l){return outerHTML.replace(result[0],'<')}
+  if(item == html_r){return outerHTML.replace(result[0],'>')}
 }
 
+
+html = `<div class="highlight-python"><div class="highlight"><pre><span class="gp">&gt;&gt;&gt; </span><span class="kn">from</span> <span class="nn">flask</span> <span class="kn">import</span> <span class="n">Flask</span><span class="p">,</span> <span class="n">url_for</span>
+<span class="gp">&gt;&gt;&gt; </span><span class="n">app</span> <span class="o">=</span> <span class="n">Flask</span><span class="p">(</span><span class="n">__name__</span><span class="p">)</span>
+<span class="gp">&gt;&gt;&gt; </span><span class="nd">@app.route</span><span class="p">(</span><span class="s">'/'</span><span class="p">)</span>
+<span class="gp">... </span><span class="k">def</span> <span class="nf">index</span><span class="p">():</span> <span class="k">pass</span>
+<span class="gp">...</span>
+<span class="gp">&gt;&gt;&gt; </span><span class="nd">@app.route</span><span class="p">(</span><span class="s">'/login'</span><span class="p">)</span>
+<span class="gp">... </span><span class="k">def</span> <span class="nf">login</span><span class="p">():</span> <span class="k">pass</span>
+<span class="gp">...</span>
+<span class="gp">&gt;&gt;&gt; </span><span class="nd">@app.route</span><span class="p">(</span><span class="s">'/user/&lt;username&gt;'</span><span class="p">)</span>
+<span class="gp">... </span><span class="k">def</span> <span class="nf">profile</span><span class="p">(</span><span class="n">username</span><span class="p">):</span> <span class="k">pass</span>
+<span class="gp">...</span>
+<span class="gp">&gt;&gt;&gt; </span><span class="k">with</span> <span class="n">app</span><span class="o">.</span><span class="n">test_request_context</span><span class="p">():</span>
+<span class="gp">... </span> <span class="k">print</span> <span class="n">url_for</span><span class="p">(</span><span class="s">'index'</span><span class="p">)</span>
+<span class="gp">... </span> <span class="k">print</span> <span class="n">url_for</span><span class="p">(</span><span class="s">'login'</span><span class="p">)</span>
+<span class="gp">... </span> <span class="k">print</span> <span class="n">url_for</span><span class="p">(</span><span class="s">'login'</span><span class="p">,</span> <span class="nb">next</span><span class="o">=</span><span class="s">'/'</span><span class="p">)</span>
+<span class="gp">... </span> <span class="k">print</span> <span class="n">url_for</span><span class="p">(</span><span class="s">'profile'</span><span class="p">,</span> <span class="n">username</span><span class="o">=</span><span class="s">'John Doe'</span><span class="p">)</span>
+<span class="gp">...</span>
+<span class="go">/</span>
+<span class="go">/login</span>
+<span class="go">/login?next=/</span>
+<span class="go">/user/John%20Doe</span>
+</pre></div>
+</div>`
+
+
+
+
+console.log(html2md(html))
