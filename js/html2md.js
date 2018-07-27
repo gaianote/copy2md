7cd04d2adf5c3b092936c1b02609354242571ba7
gaianote <gaianote@163.com>
2018-07-27 10:32:09 +0800

    fix tag b“

--- js/html2md.js
+++ js/html2md.js
@@ -1,15 +1,18 @@
 empty_node = /(?<=<.*?>)[\s]+?(?=<\.*>)///匹配html标签之间的空白字符
-h1 = /<h1.*?>([\s\S]*?)<\/h1>/
-h2 = /<h2.*?>([\s\S]*?)<\/h2>/
-h3 = /<h3.*?>([\s\S]*?)<\/h3>/
-h4 = /<h4.*?>([\s\S]*?)<\/h4>/
-h5 = /<h5.*?>([\s\S]*?)<\/h5>/
-h6 = /<h6.*?>([\s\S]*?)<\/h6>/
-li = /<li.*?>([\s\S]*?)<\/li>/
-p = /<\/p>/
+h1 = /(<h1.*?>)([\s\S]*?)(<\/h1>)/
+h2 = /(<h2.*?>)([\s\S]*?)(<\/h2>)/
+h3 = /(<h3.*?>)([\s\S]*?)(<\/h3>)/
+h4 = /(<h4.*?>)([\s\S]*?)(<\/h4>)/
+h5 = /(<h5.*?>)([\s\S]*?)(<\/h5>)/
+h6 = /(<h6.*?>)([\s\S]*?)(<\/h6>)/
+li = /(<li.*?>)([\s\S]*?)(<\/li>)/
+p = /(<p.*?>)([\s\S]*?)(<\/p>)/
+
 br = /<\/br>|<br>/
-b = /<b>(.+?)<\/b>/
-italic = /<i>(.+?)<\/i>/
+b = /(<b>)(.*?)(<\/b>)/
+strong = /(<strong>)(.+?)(<\/strong>)/
+italic = /(<i>)(.+?)(<\/i>)/
+
 img = /<img.*?src="(.*?)".*?>/
 td = /<\/td><td>|<tr>|<\/th><th>/
 tr = /<\/tr>/
@@ -23,7 +26,10 @@ other_tag = /<.*?>|<\/.*?>/
 html_r = '&gt;'
 html_l = '&lt;'
 //将所有匹配规则放入数组reg,注意排序顺序不能改变
-const reg_list = [span,empty_node,h1,h2,h3,h4,h5,h6,b,italic,li,p,br,tr,td,thead,img,link,pre,code,other_tag,html_l,html_r]
+const reg_list = [
+span,empty_node,
+h1,h2,h3,h4,h5,h6,li,
+b,strong,italic,br,tr,td,thead,img,link,pre,p,code,other_tag,html_l,html_r]
 
 
 var html2md = function(outerHTML){
@@ -37,18 +43,31 @@ var html2md = function(outerHTML){
 }
 
 var replace = function(outerHTML,result,item){
-  if(item == h1){return outerHTML.replace(result[0],'# '+ result[1].trim() + '\n')}
-  if(item == h2){return outerHTML.replace(result[0],'## '+ result[1].trim() + '\n')}
-  if(item == h3){return outerHTML.replace(result[0],'### '+ result[1].trim() + '\n')}
-  if(item == h4){return outerHTML.replace(result[0],'#### '+ result[1].trim() + '\n')}
-  if(item == h5){return outerHTML.replace(result[0],'##### '+ result[1].trim() + '\n')}
-  if(item == h6){return outerHTML.replace(result[0],'###### '+ result[1].trim() + '\n')}
-  if(item == li){return outerHTML.replace(result[0],'* '+ result[1].trim() + '\n')}
-  if(item == b){return outerHTML.replace(result[0],'**'+ result[1].trim() + '**')}
-  if(item == italic){return outerHTML.replace(result[0],'*' + result[1].trim() + '*')}
+  var block_tag = function(replace_str){
+    // p = /(<p.*?>)([\s\S]*?)(<\/p>)/
+    _result = result[0].replace(result[1],replace_str).replace(result[2],result[2].replace(/[\r|\n]/g,'').trim()).replace(result[3],'\n')
+    console.log(_result)
+    return outerHTML.replace(result[0],_result)
+  }
+  var both_tag = function(replace_str){
+    console.log(result[0],result[1],result[2],result[3])
+    _result = result[0].replace(result[1],replace_str).replace(result[2],result[2].replace(/[\r|\n]/g,'').trim()).replace(result[3],replace_str)
+    return outerHTML.replace(result[0],_result)
+  }
+  if(item == h1){return block_tag('# ')}
+  if(item == h2){return block_tag('## ')}
+  if(item == h3){return block_tag('### ')}
+  if(item == h4){return block_tag('#### ')}
+  if(item == h5){return block_tag('##### ')}
+  if(item == h6){return block_tag('###### ')}
+  if(item == li){return block_tag('* ')}
+  if(item == p){return block_tag('')}
+
+  if(item == b){return both_tag('**')}
+  if(item == strong){return both_tag('**')}
+  if(item == italic){return both_tag('*')}
   if(item == pre){return outerHTML.replace(result[0],'\n' + '```'+ '\n')}
   if(item == span){
-    console.log('span:' + outerHTML.replace(result[0],''))
     return outerHTML.replace(result[0],'')}
   if(item == code){return outerHTML.replace(result[0],'`')}
   if(item == img){
@@ -59,10 +78,9 @@ var replace = function(outerHTML,result,item){
   if(item == tr){return outerHTML.replace(result[0],'|' + '\n')}
   if(item == thead){return outerHTML.replace(result[0],'|----------|----------|' + '\n')}
   if(item == br){return outerHTML.replace(result[0],'\n')}
-  if(item == p){return outerHTML.replace(result[0],'\n')}
+
   if(item == other_tag){return outerHTML.replace(result[0],'')}
   if(item == empty_node){
-    console.log('empty_node:' + outerHTML.replace(result[0],''))
     return outerHTML.replace(result[0],'')}
   if(item == html_l){return outerHTML.replace(result[0],'<')}
   if(item == html_r){return outerHTML.replace(result[0],'>')}
