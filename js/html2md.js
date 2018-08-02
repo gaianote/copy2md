9dfbbcf3447d7fb7d02a8e21bf8bf965c1e670df
gaianote <gaianote@163.com>
2018-08-02 19:44:56 +0800

    fix pre tag

--- js/html2md.js
+++ js/html2md.js
@@ -1,91 +1,3 @@
-// empty_node = /(?<=<.*?>)[\s]+?(?=<\.*>)///匹配html标签之间的空白字符
-// h1 = /(<h1.*?>)([\s\S]*?)(<\/h1>)/
-// h2 = /(<h2.*?>)([\s\S]*?)(<\/h2>)/
-// h3 = /(<h3.*?>)([\s\S]*?)(<\/h3>)/
-// h4 = /(<h4.*?>)([\s\S]*?)(<\/h4>)/
-// h5 = /(<h5.*?>)([\s\S]*?)(<\/h5>)/
-// h6 = /(<h6.*?>)([\s\S]*?)(<\/h6>)/
-// li = /(<li.*?>)([\s\S]*?)(<\/li>)/
-// p = /(<p.*?>)([\s\S]*?)(<\/p>)/
-
-// br = /<\/br>|<br>/
-// b = /(<b>)(.*?)(<\/b>)/
-// strong = /(<strong>)(.+?)(<\/strong>)/
-// italic = /(<i>)(.+?)(<\/i>)/
-
-// img = /<img.*?src="(.*?)".*?>/
-// td = /<\/td><td>|<tr>|<\/th><th>/
-// tr = /<\/tr>/
-// thead = /<\/thead>/
-// link = /<a [\s\S]*?href="(.*?)".*?>(.+?)<\/a>/
-// //处理pre 因为pre里面的换行是有意义的
-// span = /<span.*?>|<\/span>/
-// pre = /<pre.*?>|<\/pre.*?>/
-// code = /<code.*?>|<\/code.*?>/
-// other_tag = /<.*?>|<\/.*?>/
-// html_r = '&gt;'
-// html_l = '&lt;'
-// //将所有匹配规则放入数组reg,注意排序顺序不能改变
-// const reg_list = [
-// span,empty_node,
-// h1,h2,h3,h4,h5,h6,li,
-// b,strong,italic,br,tr,td,thead,img,link,pre,p,code,other_tag,html_l,html_r]
-
-
-// var html2md = function(outerHTML){
-//   reg_list.forEach((item) => {
-//     while(outerHTML.match(item)){
-//      result = outerHTML.match(item)
-//      outerHTML = replace(outerHTML,result,item)
-//     }
-//   })
-//   return outerHTML
-// }
-
-// var replace = function(outerHTML,result,item){
-//   var block_tag = function(replace_str){
-//     // p = /(<p.*?>)([\s\S]*?)(<\/p>)/
-//     _result = result[0].replace(result[1],replace_str).replace(result[2],result[2].replace(/[\r|\n]/g,'').trim()).replace(result[3],'\n')
-//     console.log(_result)
-//     return outerHTML.replace(result[0],_result)
-//   }
-//   var both_tag = function(replace_str){
-//     console.log(result[0],result[1],result[2],result[3])
-//     _result = result[0].replace(result[1],replace_str).replace(result[2],result[2].replace(/[\r|\n]/g,'').trim()).replace(result[3],replace_str)
-//     return outerHTML.replace(result[0],_result)
-//   }
-//   if(item == h1){return block_tag('# ')}
-//   if(item == h2){return block_tag('## ')}
-//   if(item == h3){return block_tag('### ')}
-//   if(item == h4){return block_tag('#### ')}
-//   if(item == h5){return block_tag('##### ')}
-//   if(item == h6){return block_tag('###### ')}
-//   if(item == li){return block_tag('* ')}
-//   if(item == p){return block_tag('')}
-
-//   if(item == b){return both_tag('**')}
-//   if(item == strong){return both_tag('**')}
-//   if(item == italic){return both_tag('*')}
-//   if(item == pre){return outerHTML.replace(result[0],'\n' + '```'+ '\n')}
-//   if(item == span){
-//     return outerHTML.replace(result[0],'')}
-//   if(item == code){return outerHTML.replace(result[0],'`')}
-//   if(item == img){
-//     return outerHTML.replace(result[0],'![img](' + result[1] + ')')}
-//   if(item == link){
-//     return outerHTML.replace(result[0],'['+ result[2].trim() +'](' + result[1].trim() + ')')}
-//   if(item == td){return outerHTML.replace(result[0],'|')}
-//   if(item == tr){return outerHTML.replace(result[0],'|' + '\n')}
-//   if(item == thead){return outerHTML.replace(result[0],'|----------|----------|' + '\n')}
-//   if(item == br){return outerHTML.replace(result[0],'\n')}
-
-//   if(item == other_tag){return outerHTML.replace(result[0],'')}
-//   if(item == empty_node){
-//     return outerHTML.replace(result[0],'')}
-//   if(item == html_l){return outerHTML.replace(result[0],'<')}
-//   if(item == html_r){return outerHTML.replace(result[0],'>')}
-// }
-
 // 接受outerHTML，tagname作为参数，返回tag为elem列表
 // 可以使用elem.attr访问elem属性...
 var param_html = function(outerHTML,tagname){
@@ -148,7 +60,7 @@ return packing_elems(outerHTML,tagname)
 4. 转换&gt;等
 */
 var html2md = function(html){
-    var block_elems = {
+    var elems_tag = {
     'h1':['# ','\n'],
     'h2':['## ','\n'],
     'h3':['### ','\n'],
@@ -156,18 +68,20 @@ var html2md = function(html){
     'h5':['##### ','\n'],
     'h6':['###### ','\n'],
     'li':['* ','\n'],
-    'p':['','\n']
+    'p':['','\n'],
+    'tr':['','|\n'],
+    'td':['|',''],
+    'th':['|',''],
+    'code' : ['`','`'],
+    'b':['**','**'],
+    'i':['*','*'],
+    'strong':['**','**'],
+    'span':['',''],
+    'a':['',''],
+    'img':['',''],
+
     }
 
-    var inline_elems = {
-      'code' : '`',
-      'b':'**',
-      'i':'*',
-      'strong':'**',
-      'span':'',
-      'a':'',
-      'img':''
-    }
   //将一个block_elem替换为markdown语法的
   var param_elem_to_markdown = function(tagname,start,end){
     if (param_html(html,tagname) == null) {return false}
@@ -188,7 +102,7 @@ var html2md = function(html){
     }
     else if(tagname == 'pre'){
       for (var elem of param_html(html,tagname)){
-        if(elem.content){markdown = start + elem.content.replace(/<.*?>/g,'') + end}
+        if(elem.content){markdown = start + elem.content.replace(/<br>|<\/br>/g,'\n').replace(/<.*?>/g,'') + end}
         else{markdown = start + end}
         html = html.replace(elem.outerHTML,markdown)
       }
@@ -205,11 +119,8 @@ var html2md = function(html){
   param_elem_to_markdown('pre','```\n','\n```\n')
   empty_node = /(?<=<.*?>)[\s]+?(?=<.*?>)/g
   html = html.replace(empty_node,'')
-  for (var tagname in block_elems){
-    param_elem_to_markdown(tagname,block_elems[tagname][0],block_elems[tagname][1])
-  }
-  for (var tagname in inline_elems){
-    param_elem_to_markdown(tagname,inline_elems[tagname],inline_elems[tagname])
+  for (var tagname in elems_tag){
+    param_elem_to_markdown(tagname,elems_tag[tagname][0],elems_tag[tagname][1])
   }
   html = html.replace(/<.*?>/g,'')
   html = html.replace(/&gt;/g,'>')
